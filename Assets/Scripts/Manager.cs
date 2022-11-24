e600ca9500126fc2d37307bcc5dc55357e906ee7
Simon <100833981+Currywurst93@users.noreply.github.com>
2022-11-24 13:16:18 +0100

    Debug.Log Message bei Collision mit Objekten

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision with object");
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-
-
-
-
 
     //To Do
     //Arrays, While und If müssen eingebaut werden
     //Spieler muss von der Kanone getrackt werden
     //Score muss hochzählen, wenn der Player von einem Schneeball ausweicht
     //Spiel muss stopen, wenn der Player von einem Schneeball getroffen wurde
-    //
+
 }
