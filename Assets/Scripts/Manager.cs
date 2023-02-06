00b9e61dd92732449fccace54afbb902adea6c66
Simon Baumgartner <simonbaumgartner@MacBook-Pro-von-Simon.local>
2023-02-06 11:15:39 +0100

    Restructure Folder hierachy, multiple feature according to readme

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,31 +1,110 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Manager : MonoBehaviour
+
 {
+
+    public int score = 0;
+    public TextMeshProUGUI scoreboard;
+    public Snowball[] snowballs;
+
+    public int fail;
+
+    public AudioClip shotSound;
+    public AudioSource shotAudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LadeMagazin();
+        fail = 5;
+        shotAudioSource = GetComponent<AudioSource>();
     }
 
-    public void OnCollisionEnter(Collision collision)
+    public Snowball GibKugel()
     {
-        Debug.Log("collision with object");
+        
+
+        for (int i = 0; i < snowballs.Length; i++)
+        {
+
+
+            if (!snowballs[i].IsActiv())
+            {
+                Debug.Log("Gib Kugel " + snowballs[i].name);
+                snowballs[i].gameObject.SetActive(true);
+                return snowballs[i];
+
+
+            }
+
+        }
+        return null;
+    }
+
+    public void Snowballshot()
+    {
+        Snowball tmp = GibKugel();
+        tmp.SetSpawnPos();
+
+        tmp.SetSnowballActive();
+        //shotAudioSource.PlayOneShot(shotSound);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        scoreboard.text = "Score: " + score + " Leben: " + fail;
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Snowball tmp = GibKugel();
+            tmp.SetSpawnPos();
+
+            tmp.SetSnowballActive();
+            Debug.Log("nach setActive " + tmp.name + tmp.gameObject.activeSelf);
+        }
+
+        if (fail < 1)
+        {
+            // Debug.Log("Der Spieler hat verloren");
+            scoreboard.text = "GAME OVER Score: " + score;
+        }
     }
+
+
+
+    public void LadeMagazin()
+    {
+        for (int i = 0; i < snowballs.Length; i++)
+        {
+            snowballs[i].gameObject.SetActive(false);
+            Debug.Log(snowballs[i].IsActiv() + snowballs[i].name);
+        }
+    }
+
+    public void EntladeMagazin()
+    {
+        for (int i = 0; i < snowballs.Length; i++)
+        {
+            snowballs[i].gameObject.SetActive(true);
+            Debug.Log(snowballs[i].IsActiv() + snowballs[i].name);
+        }
+    }
+
+    
+
+}
+
+
 
     //To Do
     //Arrays, While und If müssen eingebaut werden
-    //Spieler muss von der Kanone getrackt werden
-    //Score muss hochzählen, wenn der Player von einem Schneeball ausweicht
-    //Spiel muss stopen, wenn der Player von einem Schneeball getroffen wurde
+    //Kanone muss immer wo anders hin schießen - done
+    //Score muss hochzählen, wenn der Player von einem Schneeball getroffen wird
+    //Spiel muss stopen, wenn der Player einen Schneeball nicht abblockt
 
-}
